9dd367e0675cb3cdbcf5b74e034ff534deac1e4d
gangdol2012 <140498789+gangdol2012@users.noreply.github.com>
2026-05-10 17:39:01 +0900

    Revert "fix..?"
    This reverts commit 2661ed9c57fcf75ead37ba61004d8f6b1781ff80.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,29 +1,28 @@
 /// <reference types="vite/client" />
 
 declare const __APP_VERSION__: string;
-declare const __APP_BUILD_ID__: string;
 
-declare module 'monaco-editor/esm/vs/editor/editor.worker?worker&url' {
-  const workerUrl: string;
-  export default workerUrl;
+declare module 'monaco-editor/esm/vs/editor/editor.worker?worker' {
+  const WorkerFactory: new () => Worker;
+  export default WorkerFactory;
 }
 
-declare module 'monaco-editor/esm/vs/language/json/json.worker?worker&url' {
-  const workerUrl: string;
-  export default workerUrl;
+declare module 'monaco-editor/esm/vs/language/json/json.worker?worker' {
+  const WorkerFactory: new () => Worker;
+  export default WorkerFactory;
 }
 
-declare module 'monaco-editor/esm/vs/language/css/css.worker?worker&url' {
-  const workerUrl: string;
-  export default workerUrl;
+declare module 'monaco-editor/esm/vs/language/css/css.worker?worker' {
+  const WorkerFactory: new () => Worker;
+  export default WorkerFactory;
 }
 
-declare module 'monaco-editor/esm/vs/language/html/html.worker?worker&url' {
-  const workerUrl: string;
-  export default workerUrl;
+declare module 'monaco-editor/esm/vs/language/html/html.worker?worker' {
+  const WorkerFactory: new () => Worker;
+  export default WorkerFactory;
 }
 
-declare module 'monaco-editor/esm/vs/language/typescript/ts.worker?worker&url' {
-  const workerUrl: string;
-  export default workerUrl;
+declare module 'monaco-editor/esm/vs/language/typescript/ts.worker?worker' {
+  const WorkerFactory: new () => Worker;
+  export default WorkerFactory;
 }
